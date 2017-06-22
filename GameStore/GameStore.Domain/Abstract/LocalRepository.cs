5136a5815bd140b7929f2fa0269f8da2a8e1e7ff
Ilya <filippovilya90@gmail.com>
2017-06-22 23:22:55 +0300

    add GameStore has done until Cart

---
+++ GameStore/GameStore.Domain/Abstract/LocalRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using GameStore.Domain.Entities;
+
+namespace GameStore.Domain.Abstract
+{
+	public class LocalRepository : IGameRepository
+	{
+		public IEnumerable<Game> Games
+		{
+			get
+			{
+				return new[] {
+					new Game { Name = "SimCity", Price = 1499 },
+					new Game { Name = "TITANFALL", Price = 2299 },
+					new Game { Name = "Battlefield 4", Price = 899.4M }
+				};
+			}
+		}
+	}
+}
---
+++ GameStore/GameStore.WebUI/Global.asax.cs
@@ -0,0 +1,32 @@
+﻿using GameStore.Domain.Abstract;
+using GameStore.WebUI.Controllers;
+using GameStore.WebUI.Infrastructure;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace GameStore.WebUI
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+			var container = new UnityContainer().LoadConfiguration();
+			DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+			/*
+			IUnityContainer container = new UnityContainer();
+			container.RegisterType<GameController>();
+			container.RegisterType<IGameRepository, LocalRepository>();
+			DependencyResolver.SetResolver(new DemoUnityDependencyResolver(container));
+			*/
+		}
+    }
+}
